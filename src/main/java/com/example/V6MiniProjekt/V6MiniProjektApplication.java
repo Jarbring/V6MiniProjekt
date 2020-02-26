077278d622268260517730ad91edb4704380719b
Jarbring <sevein404@gmail.com>
2020-02-26 10:03:25 +0100

    Initial Commit

---
+++ src/main/java/com/example/V6MiniProjekt/V6MiniProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.V6MiniProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V6MiniProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V6MiniProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/V6MiniProjekt/V6MiniProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.V6MiniProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V6MiniProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
