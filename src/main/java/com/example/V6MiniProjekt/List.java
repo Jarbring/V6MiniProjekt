aface6f2e16cb4517b99e8309ece8dede883dfd0
AngelicaHarryson <angelica.harryson@gmail.com>
2020-02-26 14:25:10 +0100

    Skapade klasser och hårdkodade listor

--- src/main/java/com/example/V6MiniProjekt/List.java
+++ src/main/java/com/example/V6MiniProjekt/List.java
@@ -1,13 +1,37 @@
 package com.example.V6MiniProjekt;
 
 public class List {
-    public String name;
+    public String listName;
     public long id;
     public String tag;
 
-    public List(String name, long id, String tag) {
-        this.name = name;
+    public List(String listName, long id, String tag) {
+        this.listName = listName;
         this.id = id;
+        this.tag = tag;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
         this.tag = tag;
     }
 }
