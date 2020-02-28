82054cf39a3bd3e042350796afc515bea25f9e98
oskarborbos <oskar.borbos@gmail.com>
2020-02-28 14:54:39 +0100

    tagit bort onödigt

--- src/main/java/com/example/V6MiniProjekt/ListRepository.java
+++ src/main/java/com/example/V6MiniProjekt/ListRepository.java
@@ -23,7 +23,6 @@ public class ListRepository {
 
     public HashMap<String, List<String>> getHm() {
 
-
         hm.put("Mikael", mikael);
         hm.put("Jonathan", jonathan);
         hm.put("Oskar", oskar);
@@ -46,24 +45,4 @@ public class ListRepository {
 
         return accountList;
     }
-
-    public List<Music> getMusicList() {
-        List<Music> musicList = new ArrayList<>();
-        musicList.add(new Music("MusicList", 1, "music", "Artist1", "song1", "Album1", "Pop", 1994));
-        musicList.add(new Music("MusicList", 2, "music", "Artist2", "song2", "Album2", "Rock", 2006));
-        musicList.add(new Music("MusicList", 3, "music", "Artist3", "song3", "Album3", "Hiphop", 2018));
-
-        return musicList;
-    }
-
-    public List<TVSeries> getTVSeriesList() {
-        List<TVSeries> tvSeriesList = new ArrayList<>();
-        tvSeriesList.add(new TVSeries("TVSeriesList", 1, "TVSeries", "Series1", "Adventure", "Director1", 2006));
-        tvSeriesList.add(new TVSeries("TVSeriesList", 2, "TVSeries", "Series2", "History", "Director2", 1993));
-        tvSeriesList.add(new TVSeries("TVSeriesList", 3, "TVSeries", "Series3", "Romance", "Director3", 2014));
-
-        return tvSeriesList;
-    }
-
-
 }
